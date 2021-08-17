8db6218db7a4fd6f0ec7dd3b2b5a3b8cb958eecc
akshay231012 <65705290+akshay231012@users.noreply.github.com>
2021-08-17 18:36:23 +0530

    Add files via upload

---
+++ phonenofinder.py
@@ -0,0 +1,13 @@
+import phonenumbers
+from test import number
+
+# to find country name
+from phonenumbers import geocoder
+ch_number = phonenumbers.parse(number, "CH")
+print(geocoder.description_for_number(ch_number, "en"))
+
+
+# to find name of service provider
+from phonenumbers import carrier
+service_number = phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_number, "en"))
